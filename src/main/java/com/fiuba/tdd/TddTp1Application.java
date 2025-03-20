e09ba3f76e907421ebe81ee62b6726e8bb1232fd
Luciano Jadur <ljadur@medallia.com>
2025-03-20 01:13:52 -0300

    + Project base structure

---
+++ src/main/java/com/fiuba/tdd/TddTp1Application.java
@@ -0,0 +1,14 @@
+package com.fiuba.tdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddTp1Application {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TddTp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiuba/tdd/TddTp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiuba.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddTp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
